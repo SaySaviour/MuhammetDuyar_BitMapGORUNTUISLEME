b4cb6c0af31caceea67bdb0a378fd9d389020b7b
SaySaviour <49239301+SaySaviour@users.noreply.github.com>
2019-05-25 20:01:23 +0300

    Muhammet Duyar

---
+++ Muhammet_Duyar[ÖDEV2]/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Muhammet_Duyar_ÖDEV2_
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 49);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(554, 72);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Yellow;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 127);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(886, 416);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.DarkBlue;
+            this.label1.Location = new System.Drawing.Point(12, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(544, 46);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Lütfen Metin Girip Enter Tuşuna Basınız";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PapayaWhip;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(598, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(300, 72);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Font Değiştir";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(910, 555);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "RESİM";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Muhammet_Duyar[ÖDEV2]/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Muhammet_Duyar_ÖDEV2_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        FontDialog yenifont = new FontDialog();
+        private void pictureboxayazdir()
+        {
+            /*Kullanıcıdan aldığım fontu yeni bir font değişkenine atıyorum*/
+            Font kullanilacakfont = yenifont.Font;
+            /*Resim adında Formun İçindeki Picturboxın Boyutu Kadar bir bitmap piksellerini oluşturuyorum */
+            Bitmap resim = new Bitmap(this.pictureBox1.Size.Width,this.pictureBox1.Size.Height);
+            /*Oluşturmuş Olduğum Resmi Graphics nesnesi ile resme dönüştürdüm*/
+            Graphics yenigörüntü = Graphics.FromImage(resim);
+            /*Oluşturmuş olduğum yeni görüntüyü drawstring fonksiyonu ile girdiğim textboxtaki degeri seçtiğim font , renk  ve yazının konumunun ayarlanmasını sağlıyorum*/
+            yenigörüntü.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+            yenigörüntü.DrawString(textBox1.Text, kullanilacakfont, Brushes.OrangeRed, 0, 0);
+            /* En sonda ise pictureboxın içinde gösteriyorum*/
+            this.pictureBox1.Image = resim;
+            /*Debug içine direkt olarak jpg resim dosyası olarak kalmaktadır.*/
+            resim.Save("Yeni.jpg");
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show("Yazı için Bir Font Belirleyiniz","Mesaj",MessageBoxButtons.OK,MessageBoxIcon.Asterisk);
+            yenifont.ShowDialog();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Metin Girilip Enter Tuşuna Basıldığında Pictureboxa direk resim olarak atayacaktır.....
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                pictureboxayazdir();
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            yenifont.ShowDialog();
+            pictureboxayazdir();
+        }
+        /* Bitmap  bmp = new Bitmap(2480,3500);
+Şeklinde resmin width ve height değerini vererek oluşturabiliriz. Dilersek aşağıdaki kodu kullanarak oluşturduğumuz resmin pixel formatını da belirleyebiliriz.
+
+Bitmap bmp = new Bitmap(2480,3500,PixelFormat.Format24bppRgb);
+Resmi oluşturduktan sonra dpi değerini belirlemek istersek aşağıdaki kodu kullanabiliriz.
+
+bmp.SetResolution(300,300);
+Oluşturduğumuz resmin arka planını ise FillRectangle kullanarak istediğimiz renge boyayabiliriz.
+
+using (Graphics grp = Graphics.FromImage(bmp))
+{
+grp.FillRectangle( Brushes.White, 0, 0, bmp.Width, bmp.Height);
+}
+Bir bitmap nesnesini transparan yapmak istiyorsak ;
+
+bmp.MakeTransparent();
+methodunu kullanabiliriz. Bir Resmin herhangi bir pixelde rengini değiştirmek istiyorsak setpixel methodunu kullanabiliriz.Örnek olarak;
+
+Color mavi = Color.Blue;
+bmp.SetPixel(100,200,mavi);
+yaparsak yukarıda x=100 ve y=200 pixelin rengini mavi renge çevirmiş olur.
+
+Eğer oluşturduğumuz resmi kaydetmek istersek save methodunu kullanırız. Resim kaydedileceği dosya yolunu girdikten sonra nokta kullanarak resmin hangi formatta kaydedileceğini belirleriz.
+
+bmp.Save("resim.jpg"); */
+    }
+}
